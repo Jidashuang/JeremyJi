097b487d1cf5ae933a96e4137d79caaeb67ab84c
Jeremy <kidaisou@gmail.com>
2026-05-04 22:36:05 +0900

    build: enable MDX in Next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,19 @@
 import type { NextConfig } from "next";
+import createMDX from "@next/mdx";
+
+const withMDX = createMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [["remark-gfm", {}]],
+    rehypePlugins: [["rehype-slug", {}]],
+  },
+});
 
 const nextConfig: NextConfig = {
   turbopack: {
     root: __dirname,
   },
+  pageExtensions: ["ts", "tsx", "mdx"],
 };
 
-export default nextConfig;
+export default withMDX(nextConfig);
